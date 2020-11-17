bfd7b23325b08bdc6ec1b8152fbd88b0604c76b1
Michael Lavva <mlavva@berkeley.edu>
2020-11-16 20:16:12 -0800

    progress on project

---
+++ src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,55 @@
+package io.javabrains.coronavirustracker.services;
+
+import com.sun.net.httpserver.Headers;
+import io.javabrains.coronavirustracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.xml.stream.Location;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDataService {
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv";
+
+    private List<LocationStats> allStats = new ArrayList<>();
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
+    public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            LocationStats locationStat = new LocationStats();
+            String state = record.get("Province/States");
+            locationStat.setState(state);
+            String country = record.get("Country/Region");
+            locationStat.setCountry(country);
+            try {
+                int value = Integer.parseInt(record.get(record.size() - 100));
+                locationStat.setLatestTotal(value);
+            } catch (Exception e) {
+
+            }
+            System.out.println(locationStat);
+            newStats.add(locationStat);
+        }
+    }
+}
